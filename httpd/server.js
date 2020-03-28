d2b4718035226bcc2ee568ecfebcbc6411ab76ed
Juan Christian Dotulung <juandotulung@gmail.com>
2020-03-28 18:31:46 +0700

    feat, refactor: add redis session, changed serving module from http to express()

--- httpd/server.js
+++ httpd/server.js
@@ -8,17 +8,30 @@ const express = require("express"),
   session = require("express-session");
 
 const app = express();
-const redisClient = redis.createClient();
+const redisClient = redis.createClient(process.env.REDIS_URL, {
+  no_ready_check: true
+});
+const redisStore = require("connect-redis")(session);
 const port = process.env.PORT;
-const server = require("http").createServer(app);
 
 mongoose
   .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .catch(err => console.log(err));
 
 redisClient.on("error", err => {
-  console.log("Redis error: " + err);
+  console.log("❌ Redis error: " + err);
 });
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+    store: new redisStore({
+      client: redisClient
+    })
+  })
+);
 
 app.use(
   bodyParser.urlencoded({
@@ -26,13 +39,13 @@ app.use(
   })
 );
 
-if (!process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
   app.use(cors());
   app.use(morgan("dev"));
 }
 
 // Handlers
 require("./handlers/index")(app);
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`✅ Listening server on 127.0.0.1:${port}`);
 });
