2cb33097e922a0ae47c2be64e6c10f3bc329e0ef
Juan Christian Dotulung <juandotulung@gmail.com>
2020-03-30 19:11:22 +0700

    refactor(server): remove database connected log

--- server.js
+++ server.js
@@ -5,17 +5,10 @@ const port = process.env.PORT;
 const config = require('./config');
 
 mongoose.set('useCreateIndex', true);
-mongoose
-  .connect(config.database.uri, config.database.options)
-  .then(() => {
-    console.log(
-      `✅ Connected to database: ${config.database.uri}/${config.database.options.dbName}`
-    );
-  })
-  .catch((err) => {
-    console.log(`❌ Database Connection Error: ${err}`);
-    process.exit();
-  });
+mongoose.connect(config.database.uri, config.database.options).catch((err) => {
+  console.log(`❌ Database Connection Error: ${err}`);
+  process.exit();
+});
 
 server.listen(port, () => {
   console.log(`✅ Listening server on 127.0.0.1:${port}`);
