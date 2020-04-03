be72770a23c6b080a1e751aef3742b57a7b50deb
Juan Christian Dotulung <juandotulung@gmail.com>
2020-04-03 16:48:18 +0700

    feat: expanded the log when server has started

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
+const chalk = require('chalk');
 const server = require('./config/express');
 
 const port = process.env.PORT;
 const config = require('./config');
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect(config.database.uri, config.database.options).catch((err) => {
-  console.log(`❌ Database Connection Error: ${err}`);
-});
 
-server.listen(port, () => {
-  console.log('✅ Server Has Started');
-});
+mongoose
+  .connect(config.database.uri, config.database.options)
+  .then(() => {
+    console.log(chalk.greenBright('Connected to database!'));
+
+    server.listen(port, () => {
+      console.log(chalk.greenBright('Server Has Started!'), '\n');
+      if (process.env.NODE_ENV !== 'production') {
+        console.log(`
+        Server started at: ${chalk.bgMagenta(`127.0.0.1:${port}`)}
+        `);
+      }
+    });
+  })
+  .catch((err) => {
+    console.log(chalk.red(`❌ Database Connection Error: ${err}`));
+  });
