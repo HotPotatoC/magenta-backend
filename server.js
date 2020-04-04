e2b17269958602820878d479d89e011501b1f61c
Juan Christian Dotulung <juandotulung@gmail.com>
2020-04-04 17:55:28 +0700

    feat: add timer on app start

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ const server = require('./config/express');
 const port = process.env.PORT;
 const config = require('./config');
 
+console.time(chalk.greenBright('Connected to database!'));
+console.time(chalk.greenBright('Server Has Started!'));
+
 mongoose.set('useCreateIndex', true);
 
 mongoose
   .connect(config.database.uri, config.database.options)
   .then(() => {
-    console.log(chalk.greenBright('Connected to database!'));
+    console.timeEnd(chalk.greenBright('Connected to database!'));
 
     server.listen(port, () => {
-      console.log(chalk.greenBright('Server Has Started!'), '\n');
+      console.timeEnd(chalk.greenBright('Server Has Started!'));
       if (process.env.NODE_ENV !== 'production') {
         console.log(`
         Server started at: ${chalk.bgMagenta(`127.0.0.1:${port}`)}
