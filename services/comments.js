bc48fbb895c060d1ec03f4f74d742b1e6910c270
Juan Christian Dotulung <juandotulung@gmail.com>
2020-04-04 21:55:57 +0700

    refactor(api.comments): converted into promises async/await

--- services/comments.js
+++ services/comments.js
@@ -1,33 +1,37 @@
 const Comment = require('../models/Comment');
 const Post = require('../models/Post');
 
-const getCommentsByPostId = (postId, callback) => {
-  Comment.find({ post_id: postId }, (err, docs) => {
-    if (err) return callback(err, null);
-    return callback(null, docs);
+const getCommentsByPostId = (postId) => {
+  return new Promise((resolve, reject) => {
+    Comment.find({ post_id: postId }, (err, docs) => {
+      if (err) return reject(err);
+      return resolve(docs);
+    });
   });
 };
 
-const createComment = (payload, callback) => {
+const createComment = (payload) => {
   const comment = new Comment({
     user_id: payload.user_id,
     post_id: payload.post_id,
     body: payload.body,
   });
 
-  comment.save((err, product) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      Post.update(
-        { _id: payload.post_id },
-        { $push: { comments: product } },
-        (_err) => {
-          if (_err) return callback(_err, null);
-          return callback(null, product);
-        }
-      );
-    }
+  return new Promise((resolve, reject) => {
+    comment.save((err, product) => {
+      if (err) {
+        reject(err);
+      } else {
+        Post.update(
+          { _id: payload.post_id },
+          { $push: { comments: product } },
+          (_err) => {
+            if (_err) return reject(_err);
+            return resolve(product);
+          }
+        );
+      }
+    });
   });
 };
 
