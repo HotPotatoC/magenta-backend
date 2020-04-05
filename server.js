68767eb52bb2f18d022f10f8d960df5273f14858
Juan Christian Dotulung <juandotulung@gmail.com>
2020-04-05 14:55:56 +0700

    refactor(server): removed port const

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ require('dotenv').config();
 const chalk = require('chalk');
 const server = require('@config/express');
 
-const port = process.env.PORT;
-
 console.time(chalk.greenBright('Server Has Started!'));
 
-server.listen(port, () => {
+server.listen(process.env.PORT, () => {
   console.timeEnd(chalk.greenBright('Server Has Started!'));
 });
