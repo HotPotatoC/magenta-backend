0d179dc784de5e1276caaa4a457934a4682204d3
Juan Christian Dotulung <juandotulung@gmail.com>
2020-04-05 15:17:38 +0700

    feat(config): added root config

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
 require('module-alias/register');
+const path = require('path');
 const development = require('@config/env/development');
 const production = require('@config/env/production');
 
 const defaults = {
+  root: path.normalize(path.join(__dirname, '/..')),
   jwt: {
     options: {
       algorithm: 'HS256',
