64616be25985afb0aa8a9495e8def42914b5d9af
Juan Christian Dotulung <juandotulung@gmail.com>
2020-04-15 20:10:54 +0700

    refactor(services): use module alias on invalidtoken module

--- services/auth.js
+++ services/auth.js
@@ -3,9 +3,9 @@ require('module-alias/register');
 const jwt = require('jsonwebtoken');
 
 const User = require('@models/User');
+const InvalidToken = require('@models/InvalidToken');
 const { validateLogin } = require('@validation/auth');
 const config = require('@config');
-const InvalidToken = require('../models/InvalidToken');
 
 /* eslint-disable consistent-return */
 function login(email, password) {
