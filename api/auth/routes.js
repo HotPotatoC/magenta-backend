195adca020ff910fa519280b7d98d1a3c6c650a3
Juan Christian Dotulung <juandotulung@gmail.com>
2020-04-16 22:04:02 +0700

    refactor(auth): change token verify endpoint

--- api/auth/routes.js
+++ api/auth/routes.js
@@ -7,6 +7,6 @@ const tokenMiddleware = require('@middlewares/tokenMiddleware');
 router.post('/login', handler.loginHandler);
 router.post('/logout', tokenMiddleware, handler.logoutHandler);
 router.post('/register', handler.registerHandler);
-router.get('/checktoken', tokenMiddleware, handler.checkToken);
+router.get('/verify', tokenMiddleware, handler.checkToken);
 
 module.exports = router;
