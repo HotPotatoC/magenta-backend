8a246565a01a7b02cabef13afe3b68dad3b7d4bf
Juan Christian Dotulung <juandotulung@gmail.com>
2020-04-22 17:51:32 +0700

    refactor(posts): remove token middleware

--- api/posts/routes.js
+++ api/posts/routes.js
@@ -5,9 +5,9 @@ const tokenMiddleware = require('@middlewares/tokenMiddleware');
 const postHandler = require('@api/posts/postHandler');
 const commentHandler = require('@api/posts/commentHandler');
 
-router.get('/', tokenMiddleware, postHandler.getPostsHandler);
-router.get('/search', tokenMiddleware, postHandler.searchPostHandler);
-router.get('/:id', tokenMiddleware, postHandler.getSinglePostHandler);
+router.get('/', postHandler.getPostsHandler);
+router.get('/search', postHandler.searchPostHandler);
+router.get('/:id', postHandler.getSinglePostHandler);
 router.post('/', tokenMiddleware, postHandler.createPostHandler);
 router.put('/:id', tokenMiddleware, postHandler.updatePostHandler);
 router.delete('/:id', tokenMiddleware, postHandler.deletePostHandler);
