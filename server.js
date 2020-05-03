f0c9490420aa3d82256a98abbe48e910c179b4c3
Juan Christian Dotulung <juandotulung@gmail.com>
2020-05-04 00:38:16 +0700

    feat: added start-server bin

--- server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-require('module-alias/register');
-const server = require('@config/express');
-
-server.listen(process.env.PORT, () => {
-  console.log(`Server Has Started on 127.0.0.1:${process.env.PORT}`);
-});
