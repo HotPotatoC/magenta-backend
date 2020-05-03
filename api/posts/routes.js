2382cd05a7cb401fe609f3670b26af40b62cf0e1
Juan Christian Dotulung <juandotulung@gmail.com>
2020-05-03 23:25:29 +0700

    refactor(api): removed posts and comments handler files

--- api/posts/routes.js
+++ api/posts/routes.js
@@ -2,26 +2,171 @@ require('module-alias/register');
 
 const router = require('express').Router();
 const tokenMiddleware = require('@middlewares/tokenMiddleware');
-const postHandler = require('@api/posts/postHandler');
-const commentHandler = require('@api/posts/commentHandler');
+const services = require('@services');
 
-router.get('/', postHandler.getPostsHandler);
-router.get('/search', postHandler.searchPostHandler);
-router.get('/:id', postHandler.getSinglePostHandler);
-router.post('/', tokenMiddleware, postHandler.createPostHandler);
-router.put('/:id', tokenMiddleware, postHandler.updatePostHandler);
-router.delete('/:id', tokenMiddleware, postHandler.deletePostHandler);
+router.get('/', async (req, res) => {
+  try {
+    const posts = await services.posts.getAllPosts(req.query);
+
+    return res.status(200).json({
+      status: res.statusCode,
+      posts,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: res.statusCode,
+      message: 'There was a problem on our side.',
+    });
+  }
+});
+
+router.get('/search', async (req, res) => {
+  if (req.query.q) {
+    try {
+      const post = await services.posts.searchPost(req.query.q);
+
+      if (post.length < 1) {
+        return res.status(404).json({
+          status: res.statusCode,
+          message: `We couldn't find anything for ${req.query.q}`,
+        });
+      }
+
+      return res.status(200).json({
+        status: res.statusCode,
+        result: post,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: res.statusCode,
+        error,
+      });
+    }
+  } else {
+    return res.status(400).json({
+      status: res.statusCode,
+      message: 'Please provide a search query',
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    const post = await services.posts.getSinglePost(postId);
+
+    return res.status(200).json({
+      status: res.statusCode,
+      post,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: res.statusCode,
+      message: 'There was a problem on our side.',
+    });
+  }
+});
+
+router.post('/', tokenMiddleware, async (req, res) => {
+  const payload = {
+    user_id: req.session.user._id,
+    ...req.body,
+  };
+
+  try {
+    await services.posts.createPost(payload);
+
+    return res.status(201).json({
+      status: res.statusCode,
+      message: 'Successfully inserted a new post to the collection',
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: res.statusCode,
+      message: error.message,
+    });
+  }
+});
+
+router.put('/:id', tokenMiddleware, async (req, res) => {
+  const postId = req.params.id;
+  const payload = req.body;
+
+  try {
+    await services.posts.updatePost(postId, payload);
+
+    return res.status(200).json({
+      status: res.statusCode,
+      message: 'Successfully updated post',
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: res.statusCode,
+      message: error.message,
+    });
+  }
+});
+
+router.delete('/:id', tokenMiddleware, async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    const { deletedCount } = await services.posts.deletePost(postId);
+
+    return res.status(200).json({
+      status: res.statusCode,
+      message: `Successfully deleted ${deletedCount} data`,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: res.statusCode,
+      message: 'There was a problem on our side.',
+    });
+  }
+});
 
 // Comments
-router.get(
-  '/:id/comments',
-  tokenMiddleware,
-  commentHandler.getCommentsByPostHandler
-);
-router.post(
-  '/:id/comments',
-  tokenMiddleware,
-  commentHandler.createCommentHandler
-);
+router.get('/:id/comments', tokenMiddleware, async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    const comments = await services.comments.getCommentsByPostId(postId);
+
+    return res.status(200).json({
+      status: res.statusCode,
+      comments,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: res.statusCode,
+      message: 'There was a problem on our side.',
+    });
+  }
+});
+
+router.post('/:id/comments', tokenMiddleware, async (req, res) => {
+  try {
+    const { userId } = await services.auth.checkToken(res.locals.token);
+
+    const payload = {
+      user_id: userId,
+      post_id: req.params.id,
+      body: req.body.body,
+    };
+
+    await services.comments.createComment(payload);
+
+    return res.status(201).json({
+      status: res.statusCode,
+      message: `Successfully added a new comment to the post id(${req.params.id})`,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: res.statusCode,
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;
